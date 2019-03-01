c9bd6466ac6728d134022b83c3a1283e6f575aaf
Michael Rodgers <38140530+michaelrodgers@users.noreply.github.com>
2019-02-28 17:41:48 -0800

    Fixes subcategories/index.php mix up

--- subcategories.php
+++ subcategories.php
@@ -1,36 +1,32 @@
 <?php
 
-require '../inc_0700/config_inc.php'; #provides configuration, pathing, error handling, db credentials 
- 
-# SQL statement
-$sql= "SELECT * FROM p4_categories";
+require '../inc_0700/config_inc.php'; #provides configuration, pathing, error handling, db credentials
+spl_autoload_register('MyAutoLoader::NamespaceLoader');//required to load SurveySez namespace objects
+$config->metaRobots = 'no index, no follow';#never index feed pages
 #Fills <title> tag. If left empty will default to $PageTitle in config_inc.php  
-$config->titleTag = 'News Feed made with love & PHP in Seattle';
-#Fills <meta> tags.  Currently we're adding to the existing meta tags in config_inc.php
-$config->metaDescription = 'Seattle Central\'s ITC250 Class News Feeds are made with pure PHP! ' . $config->metaDescription;
-$config->metaKeywords = 'RSS,PHP,Fun,News,Big Data,Regular Expressions,'. $config->metaKeywords;
-//adds font awesome icons for arrows on pager
-$config->loadhead .= '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">';
-# END CONFIG AREA ---------------------------------------------------------- 
-get_header(); #defaults to theme header or header_inc.php
+$config->titleTag = 'News Feed Sub-Categories';
+# check variable of item passed in - if invalid data, forcibly redirect back to feeds/index.php page
+if(isset($_GET['id']) && (int)$_GET['id'] > 0){#proper data must be on querystring
+	 $myID = (int)$_GET['id']; #Convert to integer, will equate to zero if fails
+}else{
+	myRedirect(VIRTUAL_PATH . "feeds/index.php");
+}
+get_header(); 
 ?>
-<h3 align="center">Categories</h3>
-
+<h3 align="center">Sub Categories</h3>
 <?php
-#reference images for pager
-//$prev = '<img src="' . $config->virtual_path . '/images/arrow_prev.gif" border="0" />';
-//$next = '<img src="' . $config->virtual_path . '/images/arrow_next.gif" border="0" />';
-#images in this case are from font awesome
+$sql = "select * from p4_feeds where CategoryID=" . $myID;
 $prev = '<i class="fa fa-chevron-circle-left"></i>';
 $next = '<i class="fa fa-chevron-circle-right"></i>';
 # Create instance of new 'pager' class
 $myPager = new Pager(10,'',$prev,$next,'');
 $sql = $myPager->loadSQL($sql);  #load SQL, add offset
+//$sqlRSS = "select Description from sm18_News_Feeds where FeedID=" . $feedID;
 # connection comes first in mysqli (improved) function
 $result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
 if(mysqli_num_rows($result) > 0)
 {#records exist - process
-	if($myPager->showTotal()===1){$item = "category";}else{$item = "categories";}  //deal with plural
+	if($myPager->showTotal()===1){$item = "Sub category";}else{$item = "Sub categories";}  //deal with plural
     echo '<div align="center">We have ' . $myPager->showTotal() . ' ' . $item . '!</div>';
     
     echo '
@@ -38,7 +34,8 @@ if(mysqli_num_rows($result) > 0)
         <table class="table table-hover">
       <thead>
         <tr>
-          <th scope="col">Categories</th>
+          <th scope="col">Sub Categories</th>
+          <th scope="col">Description</th>
         </tr>
       </thead>
          <tbody>
@@ -50,11 +47,15 @@ if(mysqli_num_rows($result) > 0)
         
         echo '
                  <tr>
-              <td><a href="' . VIRTUAL_PATH . 'RSS/subcategories.php?id=' . (int)$row['CategoryID'] . '">' . dbOut($row['Category']) . '</a></td>
+              <td><a href="' . VIRTUAL_PATH . 'RSS/feed.php?id=' . (int)$row['FeedID'] . '">' . dbOut($row['Title']) . '</a></td>
+              <td>' . dbOut($row['Description']) . '</td>
             </tr>
             
             
         ';
+         /*echo '<div align="center"><a href="' . VIRTUAL_PATH . 'surveys/survey_view.php?id=' . (int)$row['SurveyID'] . '">' . dbOut($row['Title']) . '</a>';
+         echo '</div>';
+	*/
     }
     echo '
              </tbody>
@@ -62,9 +63,7 @@ if(mysqli_num_rows($result) > 0)
     ';
 	echo $myPager->showNAV(); # show paging nav, only if enough records	 
 }else{#no records
-    echo "<div align=center>There are currently no feeds.</div>";	
+    echo "<div align=center>There are currently no sub-categories.</div>";	
 }
 @mysqli_free_result($result);
-	
 get_footer(); #defaults to theme footer or footer_inc.php
-?>
