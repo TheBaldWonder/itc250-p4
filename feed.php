a600daff2ef158ddfaa08d04c9a1e0e5396b1944
Michael Rodgers <38140530+michaelrodgers@users.noreply.github.com>
2019-03-19 11:24:40 -0700

    Add files via upload

--- feed.php
+++ feed.php
@@ -2,11 +2,14 @@
 require '../inc_0700/config_inc.php';
 spl_autoload_register('MyAutoLoader::NamespaceLoader');
 $config->metaRobots = 'no index, no follow';
-
-
 if(isset($_GET['id']) && (int)$_GET['id'] > 0){
     get_header();
     showFeeds();
+    
+    echo '
+    <a href="feed_add.php" >Add To Feed</a><br />
+    <a href="feed_edit.php" >Edit Feed</a><br />';
+
     get_footer();    
     
 }else{
@@ -15,67 +18,71 @@ if(isset($_GET['id']) && (int)$_GET['id'] > 0){
 }
 
 function showFeeds(){ 
-    $expireAfterSeconds = 5 * 60;
-    $cache = false;
-    $myID = (int)$_GET['id'];
-    
     startSession();
+    $myID = (int)$_GET['id'];
+    $cached = false;
+    $TimeStamp = 0;
+    $Stories = array();
     
-    if(isset($_SESSION['Feeds'][$myID])){
-        $cache = true;
-        $secondsInactive = time() - $_SESSION[$myID]->TimeStamp;
-        
-        echo 'Cache from = ' . $_SESSION['Feeds'][$myID]->TimeStamp . '   Current time = ' . time() . '    difference = ' . $secondsInactive . '   ';
-        
-        if($secondsInactive > $expireAfterSeconds){
-            $_SESSION['Feeds'][$myID]->TimeStamp = time();
-            echo 'session2 = ' . $_SESSION['Feeds'][$myID]->TimeStamp;
-            $cache = false;
+    if (isset($_SESSION['Feed'])){
+        foreach($_SESSION['Feed'] as $feed){
+            if ($feed->Page == $myID){
+                $TimeStamp == $feed->TimeStamp;
+                $Stories[] = $feed->Stories;
+                $cached = true;
+            }
         }
     }
- 
-    if(!isset($_SESSION['Feeds'][$myID]) || $cache == false) {
-        echo 'NEW CACHE';
+    if ($cached == false || (time() - $TimeStamp >= 5 * 60)){
+        
+        $_SESSION['Feed'] = array();
+        
         $sql = "select FeedURL from p4_feedURLs where FeedID=" . $myID;
         $result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
                                   
         while($row = mysqli_fetch_assoc($result)){
-            $Feeds[] = dbOut($row['FeedURL']);     
+            $URLs[] = dbOut($row['FeedURL']);     
         }
         
         @mysqli_free_result($result);
         
-        foreach($Feeds as $Feed){
-            $response = file_get_contents($Feed);
+        foreach($URLs as $URL){
+            $response = file_get_contents($URL);
             $xml = simplexml_load_string($response);
             foreach($xml->channel->item as $item){
                 $myStory = new Story ($item->link, $item->title, strtotime($item->pubDate));
-                global $myStories;
                 $Stories[] = $myStory;
             }
         }
                 
         usort($Stories, 'sort_objects_by_date');
         
-        $_SESSION['Feeds'][] = array();
-        $_SESSION['Feeds'][$myID] = new Feed($myID, $Stories, time());
-                                           
+        $_SESSION['Feed'] = new Feed ($myID, time(), $Stories);
+        echo 'New Session Started at:' . time() . '<br />';
+
+    } else { // echo the already-stored data.
+        echo 'Session data exists prior to page load.  Last Cache: ' . $TimeStamp . '<br />';
     }
     
-    foreach($_SESSION['Feeds'][$myID]->Stories as $story){
+    foreach($Stories as $story){
         echo '<a href="' . $story->Link . '"><h3>' . $story->Title . '</h3></a>';
     }
-
+    # If querystring loaded, destroy session
+    if (isset($_GET['destroy'])) {
+        $_SESSION = ""; #happens inside session_eliminate(), but not if handling via file/cookie
+        session_destroy();
+    } else {# Show link to allow destuction of session
+        echo '<a href="wn19/RSS/feed.php?destroy=true">Destroy Session</a><br />';
+    }
 }
-
+ 
 function sort_objects_by_date($a, $b){
     if ($a->PubDate == $b->PubDate){
         return 0;
     }
-    
     return ($a->PubDate > $b->PubDate) ? -1 : 1;
 }
-
+    
 class Story{
     public $Link = '';
     public $Title = '';
@@ -87,16 +94,16 @@ class Story{
         $this->PubDate = $PubDate;
     }
 }
-
 class Feed{
-    public $ID = 0;
-    public $Stories = array();
+    public $Page = 0;
     public $TimeStamp = '';
+    public $Stories = array();
     
-    public function __construct($ID, $Stories, $TimeStamp){
-        $this->ID = $ID;
-        $this->Stories = $Stories;
+    public function __construct($Page, $TimeStamp, $Stories){
+        $this->Page = $Page;
         $this->TimeStamp = $TimeStamp;
+        $this->Stories = $Stories;
     }
 }
+
 ?>
---
+++ feed_edit.php
@@ -0,0 +1,124 @@
+<?php
+require '../inc_0700/config_inc.php';
+get_header();
+    
+    echo '
+    <a href="feed_add.php" >Add To Feed</a><br />
+    <a href="feed_edit.php" >Edit Feed</a><br />
+    <h3 align="center">Edit RSS Feed</h3>';
+
+if (isset($_POST['update'])){
+    $iConn = IDB::conn();
+    $sql = sprintf("UPDATE p4_feedURLs set FeedURL='%s' WHERE UrlID=%d",$_POST['newURL'],(int)$_POST['UrlID']);
+ 	
+	mysqli_query($iConn,$sql) or die(trigger_error(mysqli_error($iConn), E_USER_ERROR));
+	
+	//feedback success or failure of insert
+	if (mysqli_affected_rows($iConn) > 0){
+        echo 'This feed has been successfully updated!';
+    } else {
+        echo 'Something went wrong.  Data not updated!';
+    }
+    
+} else if (isset($_POST['edit'])){ 
+	echo '
+	<form action="' . VIRTUAL_PATH . '/RSS/feed_edit.php" method="post" onsubmit="return checkForm(this);">
+	<table align="center">
+		<tr>
+			<td align="right">URL: </td>
+			<td>
+				<input type="url" name="newURL" value="' . $_POST['FeedURL'] . '" />
+                <input type="hidden" name="UrlID" value="' . $_POST['UrlID'] . '" />
+			</td>
+		</tr>
+	';
+	echo '
+	   <tr>
+	   		<td align="center" colspan="2">
+	   			<input type="submit" value="Update Feed" />
+                <input type="hidden" name="update" value="feedback" />
+	       </td>
+	   </tr>
+	</table>    
+	</form>
+	';  
+        
+} else if (isset($_POST['cat'])){
+        echo '
+        <form action="' . VIRTUAL_PATH . '/RSS/feed_edit.php" method="post" onsubmit="return checkForm(this);">
+        <table align="center">
+            <tr>
+                <td align="right" style="padding-right: 1em;">Select URL:  </td>
+                <td>
+                ';
+                
+                $sql = "SELECT * FROM p4_feedURLs WHERE FeedID='{$_POST['FeedID']}'";
+                $result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
+                while($row = mysqli_fetch_assoc($result)){
+        
+                    echo '
+                    <input type="radio" name="FeedURL" value="' . $row['FeedURL'] . '" />' . dbOut($row['FeedURL']) . '<br />
+                    <input type="hidden" name="UrlID" value="' . $row['UrlID'] . '" />';
+                }
+                
+        echo '
+                </td>
+            </tr>
+	   		<td align="center" colspan="2">
+	   			<input type="submit" value="Edit Feed" />
+                <input type="hidden" name="edit" value="feedback" />
+	   		</td>
+	   	</tr>
+        </table>    
+        </form>
+        ';
+        
+        /*
+        $iConn = IDB::conn();  
+        $FeedURL = strval($_POST['FeedURL']);
+        $sql = "INSERT INTO p4_feedURLs (FeedID, FeedURL) VALUES ('{$_POST['FeedID']}', '$FeedURL')";
+
+        @mysqli_query($iConn,$sql) or die(trigger_error(mysqli_error($iConn), E_USER_ERROR));  # insert is done here
+
+        # feedback success or failure of insert
+        if (mysqli_affected_rows($iConn) > 0){
+            feedback("Feed Added!", "notice");
+        }else{
+            feedback("Feed NOT Added!", "error");
+        }
+        */
+        
+} else { //show form - provide feedback
+
+        echo '
+        <form action="' . VIRTUAL_PATH . '/RSS/feed_edit.php" method="post" onsubmit="return checkForm(this);">
+        <table align="center">
+            <tr>
+                <td align="right" style="padding-right: 1em;">Select Category:  </td>
+                <td>
+                ';
+                
+                $sql = "SELECT * FROM p4_feeds";
+                $result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
+                while($row = mysqli_fetch_assoc($result)){
+        
+                    echo '
+                    <input type="radio" name="FeedID" value="' . (int)$row['FeedID'] . '" />' . dbOut($row['Title']) . '<br />
+                    ';
+                }
+                
+        echo '
+                </td>
+            </tr>
+	   		<td align="center" colspan="2">
+	   			<input type="submit" value="Edit Feed" />
+                <input type="hidden" name="cat" value="feedback" />
+	   		</td>
+	   	</tr>
+        </table>    
+        </form>
+        ';
+}
+    
+get_footer();
+?>
