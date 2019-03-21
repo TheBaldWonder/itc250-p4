5b99515ec476069834f916117b8b8fc50a0c58f7
AmritSSC <43659767+AmritSSC@users.noreply.github.com>
2019-03-21 12:40:55 -0700

    Added comments

--- feed.php
+++ feed.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * feed.php loads and parse xml data from various RSS feeds
  *
@@ -13,10 +12,11 @@
  * @see config_inc.php
  * @todo none
  */
-
 require '../inc_0700/config_inc.php';
 spl_autoload_register('MyAutoLoader::NamespaceLoader');
 $config->metaRobots = 'no index, no follow';
+
+//check if page has been visited before
 if(isset($_GET['id']) && (int)$_GET['id'] > 0){
     get_header();
     showFeeds();
@@ -24,21 +24,22 @@ if(isset($_GET['id']) && (int)$_GET['id'] > 0){
     echo '
     <a href="feed_add.php" >Add To Feed</a><br />
     <a href="feed_edit.php" >Edit Feed</a><br />';
-
     get_footer();    
     
-}else{
+}else{ //go to subcategories
     
     myRedirect(VIRTUAL_PATH . "RSS/subcategories.php");
 }
 
+// feed function
 function showFeeds(){ 
-    startSession();
-    $myID = (int)$_GET['id'];
-    $cached = false;
-    $TimeStamp = 0;
-    $Stories = array();
+    startSession(); //start session
+    $myID = (int)$_GET['id']; //store session ID
+    $cached = false; // store cache status
+    $TimeStamp = 0; // initialize variable
+    $Stories = array(); //initiailize array
     
+    // for session 'Feed', get stories
     if (isset($_SESSION['Feed'])){
         foreach($_SESSION['Feed'] as $feed){
             if ($feed->Page == $myID){
@@ -48,7 +49,9 @@ function showFeeds(){
             }
         }
     }
-    if ($cached == false || (time() - $TimeStamp >= 5 * 60)){
+    
+    // if not cached, or time up, get new feeds from sources.
+    if ($cached == false || (time() - $TimeStamp >= 10 * 60)){
         
         $sql = "select FeedURL from p4_feedURLs where FeedID=" . $myID;
         $result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
@@ -67,19 +70,20 @@ function showFeeds(){
                 $Stories[] = $myStory;
             }
         }
-                
+        // sort stories        
         usort($Stories, 'sort_objects_by_date');
-        
+        //save stories in session
         $_SESSION['Feed'][] = new Feed ($myID, time(), $Stories);
         echo 'New Session Started at:' . time() . '<br />';
-
     } else { // echo the already-stored data.
         echo 'Session data exists prior to page load.  Last Cache: ' . $TimeStamp . '<br />';
     }
+    // return stories to webpage
     
     foreach($Stories as $story){
         echo '<a href="' . $story->Link . '"><h3>' . $story->Title . '</h3></a>';
     }
+    
     # If querystring loaded, destroy session
     if (isset($_GET['destroy'])) {
         $_SESSION = ""; #happens inside session_eliminate(), but not if handling via file/cookie
@@ -95,7 +99,8 @@ function sort_objects_by_date($a, $b){
     }
     return ($a->PubDate > $b->PubDate) ? -1 : 1;
 }
-    
+
+//class Story
 class Story{
     public $Link = '';
     public $Title = '';
@@ -107,6 +112,8 @@ class Story{
         $this->PubDate = $PubDate;
     }
 }
+
+// class Feed
 class Feed{
     public $Page = 0;
     public $TimeStamp = '';
@@ -118,5 +125,4 @@ class Feed{
         $this->Stories = $Stories;
     }
 }
-
 ?>
